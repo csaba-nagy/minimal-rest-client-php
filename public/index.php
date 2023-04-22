3de8a9a6191ab692559238733e5afa856fcb36d6
Csaba Nagy <n.csaba1991@gmail.com>
2023-04-22 10:53:54 +0000

    feat: add interface support to the container

--- public/index.php
+++ public/index.php
@@ -8,7 +8,12 @@ use App\MinimalRestClientPhp\Container;
 use App\MinimalRestClientPhp\Application;
 
 try {
+  // TODO: Make the DI Container to singleton, it's more convenient and performant solution
   $container = new Container();
+
+  // The following line show that how an interface can be handled:
+  // $container->set(IDemo::class,DemoClass::class);
+
   $app = $container->get(Application::class);
 
   var_dump($app->main());
